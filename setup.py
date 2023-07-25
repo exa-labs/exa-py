35fa702c1d07d230aee9a251c997fdee8876f137
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2023-07-25 13:49:41 -0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='metaphor-python',
-    version='0.1.6',
+    version='0.1.7',
     description='A Python package for the Metaphor API.',
     author='Metaphor',
     author_email='hello@metaphor.systems',
