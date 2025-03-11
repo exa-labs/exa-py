3bb0ee72101386a66eecf976c262592d5680e8cf
Jeffrey Wang <jeffreyzhiyuanwang@gmail.com>
2025-03-11 10:41:22 -0700

    update user agent in api.py code

--- exa_py/api.py
+++ exa_py/api.py
@@ -788,7 +788,7 @@ class Exa:
         self,
         api_key: Optional[str],
         base_url: str = "https://api.exa.ai",
-        user_agent: str = "exa-py 1.8.9",
+        user_agent: str = "exa-py 1.9.0",
     ):
         """Initialize the Exa client with the provided API key and optional base URL and user agent.
 
