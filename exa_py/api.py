80c98a6625eb2ab1553530456216c93db62ce40c
Mark Pekala <mfpekala@gmail.com>
2025-05-15 15:03:46 -0700

    actually bump

--- exa_py/api.py
+++ exa_py/api.py
@@ -877,7 +877,7 @@ class Exa:
         self,
         api_key: Optional[str],
         base_url: str = "https://api.exa.ai",
-        user_agent: str = "exa-py 1.12.2",
+        user_agent: str = "exa-py 1.12.3",
     ):
         """Initialize the Exa client with the provided API key and optional base URL and user agent.
 
