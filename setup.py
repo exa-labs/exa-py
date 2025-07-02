662397deb922a13843e6bb3cb773633c4ec0dd43
Jacob Peddicord <jpmail17@gmail.com>
2025-07-02 13:20:00 -0700

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="exa_py",
-    version="1.14.13",
+    version="1.14.14",
     description="Python SDK for Exa API.",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
