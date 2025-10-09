598f87d0aac8d237f772138e87173d5301520e82
Mark Pekala <mfpekala@gmail.com>
2025-10-09 14:08:02 -0700

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="exa_py",
-    version="1.16.0",
+    version="1.16.1",
     description="Python SDK for Exa API.",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
