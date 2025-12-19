a22867d75a647e4b36480ae42ae89feab4969312
Miguel Brandão <miguel@exa.ai>
2025-12-19 18:09:39 +0000

    chore: bump version to 2.0.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="exa_py",
-    version="2.0.1",
+    version="2.0.2",
     description="Python SDK for Exa API.",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
