53d471ec352441708460095130157583dd15ce7b
Miguel Brandão <miguel@exa.ai>
2026-01-22 10:29:14 +0000

    bump to 2.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="exa_py",
-    version="2.1.0",
+    version="2.1.1",
     description="Python SDK for Exa API.",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
